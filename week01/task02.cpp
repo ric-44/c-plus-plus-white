c0f53761ced0e4ee2c03cde44cee742046a07617
ric-44 <ric44.droid@gmail.com>
2024-02-04 15:43:55 +0300

    resolve task02

---
+++ week01/task02.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string a, b, c;
+    cin >> a >> b >> c;
+
+    string min_string = a;
+    if (b < min_string)
+    {
+        min_string = b;
+    }
+    if (c < min_string)
+    {
+        min_string = c;
+    }
+
+    cout << min_string << endl;
+
+    return 0;
+}
