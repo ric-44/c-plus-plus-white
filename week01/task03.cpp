3494c6beea333ffbaef37e637402ddf035f358fd
ric-44 <ric44.droid@gmail.com>
2024-02-04 16:23:35 +0300

    cleanup code format

--- week01/task03.cpp
+++ week01/task03.cpp
@@ -1,30 +1,24 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-    double a, b, c;
-    cin >> a >> b >> c;
+int main() {
+  double a, b, c;
+  cin >> a >> b >> c;
 
-    if (a == 0)
-    {
-        if (b != 0)
-        {
-            cout << -c / b << endl;
-        }
-        return 0;
+  if (a == 0) {
+    if (b != 0) {
+      cout << -c / b << endl;
     }
-
-    double d = b * b - 4 * a * c;
-    if (d > 0)
-    {
-        cout << (-b + sqrt(d)) / (2 * a) << ' ' << (-b - sqrt(d)) / (2 * a) << endl;
-    }
-    else if (d == 0)
-    {
-        cout << -b / (2 * a) << endl;
-    }
-
     return 0;
+  }
+
+  double d = b * b - 4 * a * c;
+  if (d > 0) {
+    cout << (-b + sqrt(d)) / (2 * a) << ' ' << (-b - sqrt(d)) / (2 * a) << endl;
+  } else if (d == 0) {
+    cout << -b / (2 * a) << endl;
+  }
+
+  return 0;
 }
